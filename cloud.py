6879cccb82362039fd5caa522363edfdcef6e88a
tangkq <tangkq@qq.com>
2022-03-30 16:26:38 +0800

    Add files via upload

---
+++ cloud.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+import random
+import time
+
+from leancloud import Engine
+from leancloud import LeanEngineError
+import leancloud
+
+engine = Engine()
+
+
+@engine.define
+def get_server_time(**params):
+    return int(time.time())
+
+@engine.define
+def match_player_record(**params):
+    heronum = params.get("heronum") #随从个数
+    version = params.get("version") #版本号
+    findnum = params.get("findnum") #需要记录条数
+    query = leancloud.Query("RecordList")
+    query.equal_to("heronum",heronum)
+    query.equal_to("version",version)
+    query.descending("createdAt")
+    query.limit(100)
+    results = query.find()
+    retcount= len(results)
+    findnum = min(findnum, retcount)
+    matches = []
+    for i in range(findnum):
+        ran = random.randint(1,len(results))
+        ret = results.pop(ran-1)
+        matches.append(ret)
+    return matches
