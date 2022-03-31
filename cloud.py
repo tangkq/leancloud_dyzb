c78ca4c1b2533eb978cafc564e79772a227f2b58
tangkq@qq.com <tangkq@qq.com>
2022-03-31 17:34:30 +0800

    3

--- cloud.py
+++ cloud.py
@@ -18,6 +18,7 @@ def match_player_record(**params):
     heronum = params.get("heronum") #随从个数
     version = params.get("version") #版本号
     findnum = params.get("findnum") #需要记录条数
+    print("match_player_record",heronum,version,findnum)
     query = leancloud.Query("RecordList")
     query.equal_to("heronum",heronum)
     query.equal_to("version",version)
@@ -32,3 +33,11 @@ def match_player_record(**params):
         ret = results.pop(ran-1)
         matches.append(ret)
     return matches
+
+@engine.define
+def match_todo(**params):
+    query = leancloud.Query("Todo")
+    query.descending("createdAt")
+    query.limit(100)
+    results = query.find()
+    return results
